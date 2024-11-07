1674d5d92c77abd2599627e0c90110b66e4e2818
aergid <develer@gmail.com>
2024-11-07 15:05:55 +0100

    Add a note on deleting buffers from telescope

--- config/plugins/telescope.nix
+++ config/plugins/telescope.nix
@@ -17,6 +17,8 @@
     };
 
     settings.defaults = {
+      # Alt + d for delete selected buffer instead of C-d
+      # https://github.com/elythh/nixvim/blob/97cb9a6b551227469ad0337c3c87f7b4ea3de207/config/plug/ui/telescope.nix#L34
       file_ignore_patterns = [
         "^.git/"
         "^.mypy_cache/"
